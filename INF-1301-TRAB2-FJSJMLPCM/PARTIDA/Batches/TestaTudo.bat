02dcb93f02d5c7a7dd6fdf0ae03ec1c9be40dcf5
PedroCMonteiro <38916855+PedroCMonteiro@users.noreply.github.com>
2018-05-17 17:27:06 -0300

    Update TestaTudo.bat

--- INF-1301-TRAB2-FJSJMLPCM/PARTIDA/Batches/TestaTudo.bat
+++ INF-1301-TRAB2-FJSJMLPCM/PARTIDA/Batches/TestaTudo.bat
@@ -1,10 +1,10 @@
-REM Executa os testes de todos os scripts através dos constructos .exe
+REM Executa os testes de todos os scripts atravÃ©s dos constructos .exe
 
 cls
 
 if exist ..\Scripts\estatisticas.estat  del ..\Scripts\estatisticas.estat
 
-..\Produtos\TRAB2-2	/s..\Scripts\TesteLista.script /l..\Produtos\TesteLista.err /a..\Scripts\estatisticas.estat
+..\Produtos\TRAB2-4	/s..\Scripts\TestePartida.script /l..\Produtos\TestePartida.err /a..\Scripts\estatisticas.estat
 if errorlevel 4 goto sai
 
 ..\Ferramentas\exbestat /e..\Scripts\estatisticas.estat
